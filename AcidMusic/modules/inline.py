4e8c0e14dad42747fdfed3276351158c7e1541d4
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-07 23:24:23 +0700

    Update inline.py

--- AcidMusic/modules/inline.py
+++ AcidMusic/modules/inline.py
@@ -21,7 +21,7 @@ from pyrogram.handlers import InlineQueryHandler
 from youtubesearchpython import VideosSearch
 from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, errors
-from AcidMusic.config import Config
+from AcidMusic import Config
 
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
