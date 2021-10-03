8d371596a2c3668e08b4ea3bc0a3be4bf639083e
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-03 12:30:41 +0700

    Update rm_download.py

--- AcidMusic/modules/rm_download.py
+++ AcidMusic/modules/rm_download.py
@@ -1,8 +1,8 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from helpers.filters import command
-from helpers.decorators import sudo_users_only, errors
+from Acidmusic.helpers.filters import command
+from AcidMusic.helpers.decorators import sudo_users_only, errors
 
 downloads = os.path.realpath("downloads")
 raw = os.path.realpath("raw_files")
