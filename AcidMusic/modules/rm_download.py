9426ced74c7b023cfbb8cbbc375bc8b4a37a8a75
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-03 12:46:11 +0700

    Update rm_download.py

--- AcidMusic/modules/rm_download.py
+++ AcidMusic/modules/rm_download.py
@@ -2,14 +2,13 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from AcidMusic.helpers.filters import command
-from AcidMusic.helpers.decorators import sudo_users_only, errors
+from AcidMusic.helpers.decorators import errors
 
 downloads = os.path.realpath("downloads")
 raw = os.path.realpath("raw_files")
 
 @Client.on_message(command(["rmd", "deletebokep"]) & ~filters.edited)
 @errors
-@sudo_users_only
 async def clear_downloads(_, message: Message):
     ls_dir = os.listdir(downloads)
     if ls_dir:
