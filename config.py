87bcec89271c61ee3499f2e43df5cd7b7f4b7973
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-06 17:01:05 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,15 +1,32 @@
+import os
 from os import getenv
-
 from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "idzeroid_bot")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Kennedy Music")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/18d25616d9883400af112.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/0be47d373adcd48cff2fb.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/91f096ffdf3a67424065c.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/d7a2a816ff3a90eeb511f.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME", "KenzxMusicBot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "kennedyassistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "kenbotsupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KennedyProject")
+OWNER_NAME = getenv("OWNER_NAME", "xgothboi") # isi dengan username kamu tanpa simbol @
+DEV_NAME = getenv("DEV_NAME", "xgothboi")
+PMPERMIT = getenv("PMPERMIT", "ENABLE")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "30"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
