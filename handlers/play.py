bd8cc05f36a04cef3bfb4b8aa2302556b62cef0f
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-06 15:01:27 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -398,7 +398,7 @@ async def m_cb(b, cb):
         else:
             await cb.answer('Chat is not connected!', show_alert=True)
 
-@Client.on_message(command(["play", f"@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command("play") & other_filters)
 async def play(_, message: Message):
     global que
     global useer
