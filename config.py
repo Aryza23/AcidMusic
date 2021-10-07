38a62b8afa02d050c83f6189d84973d45044b1a6
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-07 21:23:10 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -9,23 +9,23 @@ load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Kennedy Music")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/18d25616d9883400af112.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/0be47d373adcd48cff2fb.jpg")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/91f096ffdf3a67424065c.jpg")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/d7a2a816ff3a90eeb511f.jpg")
+BOT_NAME = getenv("BOT_NAME", "ElrixXMusic")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ccdb7dd3392bc90248472.jpg")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/257c2b28860112a84d985.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/257c2b28860112a84d985.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/257c2b28860112a84d985.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "KenzxMusicBot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "kennedyassistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "kenbotsupport")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KennedyProject")
-OWNER_NAME = getenv("OWNER_NAME", "xgothboi") # isi dengan username kamu tanpa simbol @
-DEV_NAME = getenv("DEV_NAME", "xgothboi")
-PMPERMIT = getenv("PMPERMIT", "ENABLE")
+BOT_USERNAME = getenv("BOT_USERNAME", "ELrixXBot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "ElrixXAssistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Dramaa_Club")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Sanki_BOTs")
+OWNER_NAME = getenv("OWNER_NAME", "Oo_Elrix") # isi dengan username kamu tanpa simbol @
+DEV_NAME = getenv("DEV_NAME", "Oo_Elrix")
+PMPERMIT = getenv("PMPERMIT", None)
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "30"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
