bc67299e40847ad15fd3018f1bd90066ebbc225b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-21 08:08:15 +0700

    Add files via upload

---
+++ handlers/start.py
@@ -0,0 +1,89 @@
+from time import time
+from datetime import datetime
+from config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, OWNER_NAME, UPDATES_CHANNEL, GROUP_SUPPORT
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from helpers.decorators import authorized_users_only
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        f"""✅ **Bot Is Running**\n<b>⚡ **Uptime :**</b> `{uptime}`""",
+        reply_markup=InlineKeyboardMarkup(
+            [                
+                    InlineKeyboardButton(
+                        "Start Bot", callback_data="cbstart"
+                    )                
+            ]
+        )
+    )
+
+@Client.on_message(
+    command(["helpmusic", f"helpmusic@idzeroid_bot"]) & filters.group & ~filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 **Hello** {message.from_user.mention()}</b>
+
+**Please press the button below to read the explanation and see the list of available commands !**
+
+⚡ __Powered by Aries Music A.I__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="❔ HOW TO USE ME", callback_data="cbguide")]]
+        ),
+    )
+
+
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "`PONG !!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} Ms`"
+    )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@authorized_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 Bot Status :\n"
+        f"• **Uptime :** `{uptime}`\n"
+        f"• **Start Time :** `{START_TIME_ISO}`"
+        f"• __**Powered By : IdzXartez**__"
+    )
