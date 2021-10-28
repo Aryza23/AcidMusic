953ffc8a40deb936d66c63bd12467bde3fa4bd0f
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-28 22:57:08 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -33,52 +33,6 @@ async def delcmd(_, message: Message):
        await message.delete()
     await message.continue_propagation()
 
-@Client.on_message(filters.command("reload"))
-async def update_admin(client, message: Message):
-    chat_id = get_chat_id(message.chat)
-    set(
-        chat_id,
-        [
-            member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
-    )
-    await message.reply_photo(
-                photo=f"https://telegra.ph/file/ad4eca95908b25fafe7f2.jpg",
-              caption=f"**✅ Reload Sukses __admin telah di update...__**",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Support", url=f"https://t.me/idzeroidsupport"),
-                    InlineKeyboardButton("Owner", url=f"https://t.me/idzxartez")
-                ]
-            ]
-        )
-   )
-
-@Client.on_message(filters.command("cache"))
-@errors
-async def admincache(client, message: Message):
-    set(
-        message.chat.id,
-        [
-            member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
-    )
-    await message.reply_photo(
-                photo=f"https://telegra.ph/file/ad4eca95908b25fafe7f2.jpg",
-              caption=f"**♻ Refresh Cache Sukses __admin cache di segarkan...__**",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Support", url=f"https://t.me/idzeroidsupport"),
-                    InlineKeyboardButton("Owner", url=f"https://t.me/idzxartez"),
-                ]
-            ]
-        )
-   )
-
 @Client.on_message(command(["pause", f"pause@{BOT_USERNAME}"]) & other_filters)
 @errors
 @authorized_users_only
