13efa95e068a91493d8a4424cbf9dad31faa5f4b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-28 22:11:01 +0700

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,3 +1,4 @@
+
 # Calls Music 1 - Telegram bot for streaming audio in group calls
 # Copyright (C) 2021  Roj Serbest
 
@@ -15,14 +16,16 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from typing import Union, List
+from typing import List, Union
 
 from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
 
 
 def command(commands: Union[str, List[str]]):
